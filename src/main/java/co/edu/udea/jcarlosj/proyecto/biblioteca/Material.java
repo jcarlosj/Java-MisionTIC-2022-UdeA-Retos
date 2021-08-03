97f2f3ea2899678b73ed32d3d89868ff385f3163
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-08-02 19:13:59 -0500

    [.] Clase: Define Material

---
+++ src/main/java/co/edu/udea/jcarlosj/proyecto/biblioteca/Material.java
@@ -0,0 +1,62 @@
+package co.edu.udea.jcarlosj.proyecto.biblioteca;
+
+public class Material {
+    // Atributos
+    private final int ID;
+    private String title;
+    private String code;
+    private String author;
+    private int year;
+
+    // Constructor
+    public Material( int id, String title, String code, String author, int year ) {
+        this .ID = id;
+        this.title = title;
+        this.code = code;
+        this.author = author;
+        this.year = year;
+    }
+
+    // Getters and setters
+    public int getID() {
+        return this .ID;
+    }
+
+    public String getTitle() {
+        return this .title;
+    }
+
+    public void setTitle( String title ) {
+        this .title = title;
+    }
+
+    public String getCode() {
+        return this .code;
+    }
+
+    public void setCode( String code ) {
+        this .code = code;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor( String author ) {
+        this .author = author;
+    }
+
+    public int getYear() {
+        return this .year;
+    }
+
+    public void setYear( int year ) {
+        this .year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Material [ ID: " + ID + ", author: " + author + ", code: " + code + ", title: " + title + ", year: " + year + " ]";
+    }
+
+}
