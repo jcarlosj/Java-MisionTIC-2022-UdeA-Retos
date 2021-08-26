0ce4250e527676de8a34644d4cf244ee460c5737
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-08-26 16:04:10 -0500

    [+] Reto 4 - Variante 1: Define funcionalidades Clase Nomina

--- src/main/java/co/edu/udea/jcarlosj/reto_4/Nomina.java
+++ src/main/java/co/edu/udea/jcarlosj/reto_4/Nomina.java
@@ -5,30 +5,98 @@ import java.util.ArrayList;
 public class Nomina {
     // Atributos
     private ArrayList<Trabajador> trabajadores;
+    private Trabajador trabajadorSeleccionado;
 
     // Constructor
     public Nomina() {
         this .trabajadores = new ArrayList<Trabajador>();
+        this .trabajadorSeleccionado = null;
     }
 
     // Metodos
-    public void agregarTrabajador( Trabajador trabajador ) {}
+    private void buscar( String cC ) {
 
-    public void eliminarTrabajador( String trabajador ) {}
+        for( Trabajador trabajador : this .trabajadores ) {
+            // Verifica que el # de identificacion del trabajador que se requiere buscar en la lista
+            if( trabajador .getCC() .equals( cC ) ) {
+                this .trabajadorSeleccionado = trabajador;
+            }
+        }
 
-    public double calcularSalarioQuincenaNomina( String trabajador ) {}
+    }
 
-    public double promedioEdadNomina() {}
+    public void agregarTrabajador( Trabajador t ) {
+        this .buscar( t .getCC() );      // Busca un Trabajador
 
-    public double desviacionEstandarEdadNomina() {}
+        // Verifica que no exista un trabajador con el mismo # de identificacion
+        if( this .trabajadorSeleccionado == null ) {
+            this .trabajadores .add( t );
+        }
 
-    public double salarioQuincenalTrabajador( String trabajador ) {}
+        this .trabajadorSeleccionado = null;
+
+    }
+
+    public void eliminarTrabajador( String ID ) {
+        this .buscar( ID );                 // Busca un Trabajador
+
+        // Verifica que existe un trabajador con # de identificacion
+        if( this .trabajadorSeleccionado != null ) {
+            this .trabajadores .remove( this .trabajadorSeleccionado );
+            this .trabajadorSeleccionado = null;
+        }
+
+    }
+
+    public double calcularSalarioQuincenaNomina() {
+        double salariosMensuales = 0.0;
+
+        for( Trabajador trabajador : this .trabajadores )
+            salariosMensuales += trabajador .salarioQuincenal();
+
+        return salariosMensuales;
+    }
+
+    public double promedioEdadNomina() {
+        int sumatoriaEdades = 0;
+
+        for( Trabajador trabajador : this .trabajadores )
+            sumatoriaEdades += trabajador. calcularEdad();
+
+        return sumatoriaEdades / this .trabajadores .size();
+    }
+
+    public double desviacionEstandarEdadNomina() {
+        double total = 0.0;
+
+        for( Trabajador trabajador : this .trabajadores ) {
+            total += Math .pow( trabajador .calcularEdad() - this .promedioEdadNomina(), 2 );
+        }
+
+        return Math .sqrt( ( 1 / (double) this .trabajadores .size() ) * total );
+    }
+
+    public double salarioQuincenalTrabajador( String ID ) {
+        double salarioQuincenal = -1;
+
+        this .buscar( ID );                 // Busca un Trabajador
+
+        // Verifica que existe un trabajador con # de identificacion
+        if( this .trabajadorSeleccionado != null ) {
+            salarioQuincenal = this .trabajadorSeleccionado .salarioQuincenal();
+            this .trabajadorSeleccionado = null;
+        }
+
+        return salarioQuincenal;
+    }
 
     @Override
     public String toString() {
         return
-            "Nomina [ " +
-            "\n  trabajadores: " + this .trabajadores + "\n]";
+            "\ntrabajadorSeleccionado: " + this .trabajadorSeleccionado +
+            "\nNomina { " +
+            "\n  trabajadores: " + this .trabajadores +
+            "\n}";
     }
 
 }
