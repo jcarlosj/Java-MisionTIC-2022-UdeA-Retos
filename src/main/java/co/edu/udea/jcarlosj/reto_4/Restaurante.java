96bffbe5c895bfa7ac2f5a1a11e2134d22ffdd44
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-08-26 21:25:23 -0500

    [+] Reto 4 - Variante 2: Define funcionalidades de la clase Restaurante

--- src/main/java/co/edu/udea/jcarlosj/reto_4/Restaurante.java
+++ src/main/java/co/edu/udea/jcarlosj/reto_4/Restaurante.java
@@ -25,24 +25,84 @@ public class Restaurante {
     }
 
     // Metodos
-    public void agregarPedidoLista( Pedido p ) {}
+    private void buscar( String nPedido ) {
 
-    public void eliminarPedido( String nPedido ) {}
+        for( Pedido pedido : this .pedidos ) {
+            // Verifica que el # de pedido que se requiere buscar en la lista
+            if( pedido .getnPedido() .equals( nPedido ) ) {
+                this .pedidoSeleccionado = pedido;
+            }
 
-    public double calcularGanancias() {}
+        }
 
-    public double promedioGananciasTotales() {}
+    }
 
-    public double desviacionEstandarGananciasTotales() {}
+    public void agregarPedidoLista( Pedido p ) {
+        this .buscar( p .getnPedido() );      // Busca un Pedido
 
-    public double retornarCostoPedido( double ID ) {}
+        // Verifica que no exista un pedido con el mismo # de pedido
+        if( this .pedidoSeleccionado == null ) {
+            this .pedidos .add( p );
+        }
+
+        this .pedidoSeleccionado = null;
+
+    }
+
+    public void eliminarPedido( String ID ) {
+        this .buscar( ID );                 // Busca un Pedido
+
+        // Verifica que existe un pedido con # de pedido
+        if( this .pedidoSeleccionado != null ) {
+            this .pedidos .remove( this .pedidoSeleccionado );
+            this .pedidoSeleccionado = null;
+        }
+    }
+
+    public double calcularGanancias() {
+        double costoTodosPedidos = 0.0;
+
+        for( Pedido pedido : this .pedidos )
+            costoTodosPedidos += pedido .getCostoPedido();
+
+        return costoTodosPedidos;
+    }
+
+    public double promedioGananciasTotales() {
+
+        return this .calcularGanancias() / this .pedidos .size();
+    }
+
+    public double desviacionEstandarGananciasTotales() {
+        double total = 0.0;
+
+        for( Pedido pedido : this .pedidos ) {
+            total += Math .pow( pedido .getCostoPedido() - this .promedioGananciasTotales(), 2 );
+        }
+
+        return Math .sqrt( ( 1 / (double) this .pedidos .size() ) * total );
+    }
+
+    public double retornarCostoPedido( String ID ) {
+        double costoPedido = -1;
+
+        this .buscar( ID );                 // Busca un Trabajador
+
+        // Verifica que existe un trabajador con # de identificacion
+        if( this .pedidoSeleccionado != null ) {
+            costoPedido = this .pedidoSeleccionado .getCostoPedido();
+            this .pedidoSeleccionado = null;
+        }
+
+        return costoPedido;
+    }
 
 
     @Override
     public String toString() {
         return
             "\npedidoSeleccionado: " + this .pedidoSeleccionado +
-            "Restaurante { " +
+            "\nRestaurante { " +
             "\n  pedidos: " + pedidos +
             "\n}";
     }
