39d10a71ae69c3ea8900ee13d2bb15e3ab600797
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-08-26 21:29:19 -0500

    [+] Reto 4 - Variante 2: Pruebas del dominio del problema

--- src/main/java/co/edu/udea/jcarlosj/reto_4/StartVariante2.java
+++ src/main/java/co/edu/udea/jcarlosj/reto_4/StartVariante2.java
@@ -5,7 +5,20 @@ public class StartVariante2 {
         Restaurante sopasysopitas = new Restaurante();
         System.out.println( sopasysopitas );
 
-        Pedido pedido1 = new Pedido( "3492", "7987823", 340, 26, 8, 2021 );
-        System.out.println( pedido1 );
+        Pedido pedido1 = new Pedido( "3492", "7987823", 340, 26, 7, 2021 );
+        Pedido pedido2 = new Pedido( "3493", "7987824", 210, 26, 8, 2021 );
+        Pedido pedido3 = new Pedido( "3494", "7987824", 40, 26, 9, 2021 );
+
+        sopasysopitas .agregarPedidoLista( pedido1 );
+        sopasysopitas .agregarPedidoLista( pedido2 );
+        sopasysopitas .agregarPedidoLista( pedido3 );
+        System.out.println( sopasysopitas );
+
+        sopasysopitas .eliminarPedido( "3494" );
+        System.out.println( sopasysopitas );
+
+        System .out .println( "total ganancias de todos los pedidos: " + sopasysopitas .calcularGanancias() );
+        System .out .println( "promedio ganancias totales de todos los pedidos: " + sopasysopitas .promedioGananciasTotales() );
+        System .out .println( "desviacion estandar del costo de todos los pedidos: " + sopasysopitas .desviacionEstandarGananciasTotales() );
     }
 }
